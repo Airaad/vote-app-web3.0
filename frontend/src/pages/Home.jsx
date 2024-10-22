d4d0dcb7e40fbe5ac5b0bdd04dd1f9be4dd8616a
Airaad <sheikhairaad@gmail.com>
2024-10-22 06:53:57 +0530

    minor change in ui

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -8,8 +8,8 @@ export default function Home() {
   return (
     <div>
       <SlidingCarousal />
-      <VotingAnalytics />
       <Features />
+      <VotingAnalytics />
       <FAQ />
     </div>
   );
